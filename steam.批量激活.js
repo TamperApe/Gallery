ecdd9a3257600ad5f44a15bfe401871153962489
snowlab <snowlab@hotmail.com>
2018-06-28 09:43:29 +0800

    完善

--- steam.批量激活.js
+++ steam.批量激活.js
@@ -45,8 +45,11 @@ new class steam {
             async function test(type) {
                 console.log("test");
                 let list = await ape_getScriptValue("steamKey", true, []);
+                const msg_used = "已经被另一个 Steam 帐户激活";
+                const msg_successed = "成功激活";
+                const msg_owned = "已拥有此特惠中包含的产品";
                 for (const item of list) {
-                    let msgArray = ["成功激活", "已经拥有该游戏"];
+                    let msgArray = [msg_successed, msg_owned, msg_used];
                     if (item.result) {
                         let found = false;
                         for (const msgItem of msgArray) {
@@ -70,14 +73,17 @@ new class steam {
                             //继续等待
                             return false;
 
-                        if (error.indexOf("已拥有此特惠中包含的产品") > 0) {
-                            item.result = "已经拥有该游戏";
+                        if (error.indexOf(msg_owned) > 0) {
+                            item.result = msg_owned;
+                        }
+                        else if (error.indexOf(msg_used) > 0) {
+                            item.result = msg_used;
                         }
                         else
                             item.result = error
 
                         if (success)
-                            item.result = "成功激活";
+                            item.result = msg_successed;
 
                         return true;
                     }, 1000 * 30);
